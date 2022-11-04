16b3a4d5a5368a8cf74462a64e2351f66f8564d0
Juan Carlos <jkloaiza19@gmail.com>
2022-11-04 08:12:36 -0600

    Update host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'juank.com', :protocol => 'https' }
+  config.action_mailer.default_url_options = { :host => 'ruby-gems-project.herokuapp.com/', :protocol => 'https' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
