a20babf8586f5b00d6fbb5719271fece790f956c
Juan Carlos <jkloaiza19@gmail.com>
2022-12-08 22:23:42 -0600

    Fixes

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class ApplicationMailer < ActionMailer::Base
-  default from: "jkloaiza19+amazon@gmail.com"
-  layout "mailer"
+  default from: 'jkloaiza19+amazon@gmail.com'
+  layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
 
   Rails.application.config.middleware.use ExceptionNotification::Rack,
                                           email: {
-                                            deliver_with: :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now
+                                            deliver_with: :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now # rubocop:disable Layout/LineLength
                                             email_prefix: '[PREFIX] ',
                                             sender_address: %("Email error" <support@jcprojectsonline.com>),
                                             exception_recipients: %w[jkloaiza19+amazon@gmail.com]
